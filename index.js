19a42b1e15eaee854a98cfaa8a970df2e52209b0
WilsonD09 <wilsond34@nycstudents.net>
2024-10-08 12:22:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,37 @@ const products = [
   { name: "Chair", price: 250, category: "Furniture", rating: 4.0 },
   { name: "Desk", price: 450, category: "Furniture", rating: 4.8 },
 ];
+
+products.forEach((product) =>
+  console.log(product.name, Math.round(product.price * 1.1))
+);
+
+const categories = [];
+products.forEach((product) => {
+  if (!categories.includes(product.category)) {
+    categories.push(product.category);
+  }
+});
+console.log("Unique Categories:", categories);
+
+const electronics = products.filter(
+  (product) => product.category === "Electronics"
+);
+console.log("Electronics:", electronics);
+
+const expensiveProducts = products.filter((product) => product.price > 300);
+console.log("Expensive Products:", expensiveProducts);
+
+const highRated = products.filter((product) => product.rating >= 4.5);
+console.log("Highly Rated Products", highRated);
+
+highlyRated = products
+  .filter((product) => product.rating >= 4.5)
+  .forEach((product) => console.log(product.name));
+
+affordableElectronics = products
+  .filter((product) => product.category === "Electronics")
+  .filter((product) => product.price < 1000)
+  .forEach((product) =>
+    console.log(product.name, product.price, product.category)
+  );
